47b3e7cab3ae8b3dfae6d5961cfd57e925a9de67
Sherief Farid <sherief@farid.cloud>
2020-01-05 19:58:34 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Person
   attr_accessor :name, :candy_lover
 
@@ -5,7 +7,6 @@ class Person
     @name = name
     @candy_lover = candy_lover
   end
-
   def tall?
     true
   end
@@ -26,4 +27,5 @@ class Person
         "not a ballin candy lover"
     end
   end
+end
 end
